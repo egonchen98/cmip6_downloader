6457899b570546969e659a928f02ef0893bf6aa8
egon <xwsever@outlook.com>
2023-05-18 19:35:18 +0800

    'v0.7'

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ import tools.colab as colab
 if __name__ == '__main__':
     config = json.loads(Path('./resources/config.json').read_text())
     database = config['Database_folder']
-    pool = ThreadPoolExecutor(max_workers=10)
 
     while True:
 
+        print('Starting downloading 10 files at a time: ....')
         records = [colab.get_1_record() for i in range(10)]
         records = list({v['url']: v for v in records}.values())
         if 'Finished' in records:
             break
 
+        pool = ThreadPoolExecutor(max_workers=10)
         # Add downloading threads
         for index, res in enumerate(records):
             pool.submit(colab.run, res, database)
